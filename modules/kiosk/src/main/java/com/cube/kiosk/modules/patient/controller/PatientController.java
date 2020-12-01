e2b6669562fa277d745607c0000dbb859619b663
卡卡西 <lmz77555@gmail.com>
2020-12-01 13:24:45 +0800

    查询门诊患者

--- modules/kiosk/src/main/java/com/cube/kiosk/modules/patient/controller/PatientController.java
+++ modules/kiosk/src/main/java/com/cube/kiosk/modules/patient/controller/PatientController.java
@@ -4,24 +4,31 @@ package com.cube.kiosk.modules.patient.controller;
 import com.cube.core.global.anno.ResponseApi;
 import com.cube.core.system.annotation.SysLog;
 import com.cube.kiosk.modules.anno.Access;
+import com.cube.kiosk.modules.common.ResponseData;
 import com.cube.kiosk.modules.common.ResponseHisData;
 import com.cube.kiosk.modules.common.model.ResultListener;
+import com.cube.kiosk.modules.common.utils.RestTemplate;
 import com.cube.kiosk.modules.patient.anno.PatientParamResolver;
 import com.cube.kiosk.modules.patient.model.HosPatientDO;
+import com.cube.kiosk.modules.patient.model.Patient;
 import com.cube.kiosk.modules.patient.model.PatientParam;
 import com.cube.kiosk.modules.patient.repository.HosPatientRepository;
+import com.cube.kiosk.modules.patient.repository.PatientRepository;
 import com.cube.kiosk.modules.patient.service.PatientService;
 import com.cube.kiosk.modules.system.entity.HospitalDO;
 import com.google.gson.Gson;
+import com.google.gson.internal.LinkedTreeMap;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.apache.commons.collections4.MapUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @RestController
@@ -35,9 +42,7 @@ public class PatientController {
     @Autowired
     private HosPatientRepository hosPatientRepository;
 
-    @ApiOperation(httpMethod = "POST",value = "查询患者西悉尼")
-    @RequestMapping("index")
-    @SysLog("查询患者信息")
+
     public String index(@RequestBody String cardNo){
         final Object[] objects = new Object[1];
         patientService.get(cardNo, new ResultListener() {
@@ -98,5 +103,73 @@ public class PatientController {
         hosPatientDO.setIdCard(MapUtils.getString(map,"postalCode"));
         hosPatientRepository.save(hosPatientDO);
         return hosPatientDO;
+    }
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private PatientRepository patientRepository;
+
+
+    @Value("${neofaith.token}")
+    private String token;
+
+    @Value("${neofaith.hosId}")
+    private String hosId;
+
+    @ApiOperation(httpMethod = "POST",value = "查询患者信息")
+    @RequestMapping("index")
+    @ResponseApi
+    public Object patient(@RequestBody String cardNo){
+        Patient patient = new Patient();
+        if(cardNo.length()>28){
+            String a = cardNo.substring(0,6);
+            String b = cardNo.substring(7,10);
+            String c = cardNo.substring(11,12);
+            String d = cardNo.substring(13,14);
+            String e = cardNo.substring(15,cardNo.length());
+            cardNo = a+b+c+d+e;
+        }
+
+        Map<String,Object> paramMap = new HashMap<>(16);
+        Gson gson = new Gson();
+        paramMap.put("cardId",cardNo);
+        paramMap.put("token", token);
+        paramMap.put("hosId", hosId);
+        String json = gson.toJson(paramMap);
+        String result = restTemplate.doPostNewHisApi(json,"his/getPatientnameInfo");
+        ResponseHisData<Object> responseHisData = gson.fromJson(result,ResponseHisData.class);
+        if(responseHisData.getCode()==1){
+            throw new RuntimeException((String) responseHisData.getResponseData());
+        }
+
+        if(responseHisData.getCode()==0){
+            LinkedTreeMap<String,Object> linkedTreeMap = (LinkedTreeMap<String, Object>) responseHisData.getResponseData();
+
+            patient.setName(linkedTreeMap.get("patientname").toString());
+            patient.setAge(linkedTreeMap.get("age").toString());
+            patient.setSex(linkedTreeMap.get("sex").toString());
+            patient.setCardNo(cardNo);
+            try{
+                patientRepository.save(patient);
+            }catch (Exception e){
+
+            }
+            //查询余额
+            paramMap.put("patientname",patient.getName());
+            paramMap.put("identitycard",patient.getCardNo());
+            paramMap.put("token", token);
+            paramMap.put("hosId", hosId);
+            json = gson.toJson(paramMap);
+            result = restTemplate.doPostNewHisApi(json,"his/getBalance");
+            responseHisData = gson.fromJson(result,ResponseHisData.class);
+            if(responseHisData.getCode()==0){
+                Map<String,Object> map = (Map<String, Object>) responseHisData.getResponseData();
+                patient.setBalance(MapUtils.getString(map,"balance"));
+            }
+
+        }
+        return patient;
     }
 }
